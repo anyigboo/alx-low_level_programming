775a1d8f4f74cee251b2985f2175306e896d736a
anyigboo <oscaranyigbo@gmail.com>
2022-10-16 09:14:59 -0700

    task 5 first commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * Main: Program entry point.
+ * Return:0 no error, non-zero if error.
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return(0);
+}
