a851afa3ff69f0a53cb1d73c008ed7ab371b2772
anyigboo <oscaranyigbo@gmail.com>
2022-10-20 07:57:00 -0700

    task 1 first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * main - entry point of the program
+  *
+  * return:0 if no error, non-zero if error
+  */
+void main ()
+{
+	printf("_putchar\n");
+	getch ();
+	return (0);
+}
