b3df0376fce127a3b1e700bc9c89c6025fdfb5b0
anyigboo <oscaranyigbo@gmail.com>
2022-10-24 14:04:11 -0700

    test that the function positive_or_negative() gives the correct output when given a case of 0.

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,26 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
+
 /**
- * main-program entry point.
- * Return:0-no error,non zero value if error.
- **/
+ * main - test that the function positive_or_negative()
+ * gives the correct output when given a case of 0.
+ *
+ * Return: 0
+ */
+
 int main(void)
 {
-	int n;
+	int i;
 
-	srand(time(0));
-	n = 0;
+	i = 0;
+	positive_or_negative(i);
 
-	if (n >  0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
-	else if (n == 0)
-		printf("%d is zero\n", n);
 	return (0);
 }
