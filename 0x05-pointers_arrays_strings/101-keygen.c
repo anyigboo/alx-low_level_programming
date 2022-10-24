fb1c8252f3192d387983615ee0816c6f60b19d70
anyigboo <oscaranyigbo@gmail.com>
2022-10-24 15:49:33 -0700

    generate random valid passwords for the program 101-crackme.

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,7 +11,6 @@
 int main(void)
 {
 	int sum = 2772;
-
 	int n;
 
 	srand(time(0));
@@ -23,6 +22,11 @@ int main(void)
 		else
 			continue;
 		if (sum - n == 0)
+		{
+			sum -= n;
+			printf("%c", n);
+		}
+		else if (sum - n - 32 > 0)
 		{
 			sum -= n;
 			printf("%c", n);
