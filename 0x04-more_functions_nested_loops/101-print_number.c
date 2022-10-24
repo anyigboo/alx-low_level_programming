a0c23e13b88c624a7b92ced8368f27347ce7150c
anyigboo <oscaranyigbo@gmail.com>
2022-10-24 13:40:43 -0700

    task 12 fifth commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,10 +1,13 @@
 #include "main.h"
 void print_integer(int m);
+
 /**
-  * print_number - a function that prints an integer.
-  * @n: An input integer
-  * Return: Nothing
-  */
+ * print_number - a function that prints an integer.
+ *
+ * @n: An input integer
+ *
+ * Return: Nothing
+ */
 void print_number(int n)
 {
 	if (n == 0)
@@ -18,13 +21,14 @@ void print_number(int n)
 		print_integer(n);
 }
 /**
-  * print_integer - A function to priting n
-  * @m: an input unsigned integer
-  * Return: Nothing
-  */
+ * print_integer - A function to priting n
+ * @m: an input unsigned integer
+ * Return: Nothing
+ */
 void print_integer(int m)
 {
 	int i = 1000000000;
+
 	for (; i >= 1; i /= 10)
 		if (m / i != 0)
 		{
