06655d58fcbfcb0ecbd846cdd223ceca5eb7a9e3
anyigboo <oscaranyigbo@gmail.com>
2022-10-24 14:34:01 -0700

    newest commit:

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * main - test that the function positive_or_negative()
- * gives the correct output when given a case of 0.
+ * main - test that the function
+ * positive_or_negative() gives the correct
+ * output when given a case of 0.
  *
  * Return: 0
  */
@@ -12,6 +13,5 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-
 	return (0);
 }
